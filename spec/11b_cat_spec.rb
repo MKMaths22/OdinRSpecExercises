f9d219ec472a0ab1c304e43da10723dfdcce08e0
MKMaths22 <120586172+MKMaths22@users.noreply.github.com>
2023-06-04 10:15:37 +0100

    Solve 11b_cat_spec Shared Examples Exercises

--- spec/11b_cat_spec.rb
+++ spec/11b_cat_spec.rb
@@ -35,17 +35,20 @@ end
 
 describe Cat do
   # Create a subject with your choice of cat name and optional breed/color.
-
+  subject(:my_cat) { described_class.new('Tom', 'some_breed', 'black') }
   # Write a test using the second shared_example to test that cat responds to
   # talk ('meow').
-  context '' do
+  context 'to check that Cat responds to talk' do
+    include_examples 'shared method name'
   end
 
   # remove the 'x' before running this test
-  xit 'is not hungry' do
+  it 'is not hungry' do
+    expect(subject).to_not be_hungry
   end
 
   # remove the 'x' before running this test
-  xit 'is hiding' do
+  it 'is hiding' do
+    expect(subject).to be_hiding
   end
 end
